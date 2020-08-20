e619b05696aba1657518b967944c3f901148fccb
Aishwarya Padmanabha <padmanabha.a@husky.neu.edu>
2020-08-19 21:43:38 -0400

    Add Docs

---
+++ docs/precache-manifest.0ea2d32e9b12392c806b8fb0971957ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e009d4f2d81c57f735073e1cf1a870",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "fe7f29c579640593d8f5",
+    "url": "/notes-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a664fb407ed53bbca443",
+    "url": "/notes-app/static/css/main.155b8280.chunk.css"
+  },
+  {
+    "revision": "fe7f29c579640593d8f5",
+    "url": "/notes-app/static/js/2.b7a116ed.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/notes-app/static/js/2.b7a116ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a664fb407ed53bbca443",
+    "url": "/notes-app/static/js/main.d4395837.chunk.js"
+  },
+  {
+    "revision": "760c688f173c6bfa1fac",
+    "url": "/notes-app/static/js/runtime-main.482a3d78.js"
+  }
+]);
